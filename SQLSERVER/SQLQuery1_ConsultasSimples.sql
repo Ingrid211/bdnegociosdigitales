5e9845b4050e1587e6e1d782348a3e76bb7997f3
Ingrid Adianel Saavedra Calva <saavedraiadianel24@gmail.com>
2023-02-15 16:42:30 -0600

    Primer commit de proyecto inicial

---
+++ SQLSERVER/SQLQuery1_ConsultasSimples.sql
@@ -0,0 +1,13 @@
+-- Listar las oficinas cuyas ventas están por debajo del 80% de sus objetivos
+select * from Oficinas
+select oficina, ciudad, region, ventas, objetivo
+from oficinas
+where ventas < (0.80 * objetivo)
+
+-- Hallar los pedidos del ultimo trimestre de 1989
+
+select * from Pedidos
+where Fecha_Pedido between '1989-10-01' and '1989-12-31'
+
+select * from Pedidos
+where Fecha_Pedido >= '1989-10-01' and Fecha_Pedido <= '1989-12-31'
